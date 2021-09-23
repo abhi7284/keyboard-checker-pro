a699ce5c7c089b092bfcc25168a742904962d4af
Abhishek Kumar <ak330569@gmail.com>
2021-09-23 16:39:53 +0530

    add files via upload

---
+++ index.js
@@ -0,0 +1,653 @@
+window.addEventListener('keydown', function (e) {
+
+    if (e.keyCode == 3) {
+        $("#break").addClass("active");
+    }
+    if (e.keyCode == 8) {
+        $("#backspace").addClass("active");
+    }
+    if (e.keyCode == 9) {
+        $("#tab").addClass("active");
+    }
+    if (e.keyCode == 12) {
+        $("#clear").addClass("active");
+    }
+    if (e.keyCode == 13) {
+        $("#enter").addClass("active");
+        $("#num-enter").addClass("active");
+    }
+    if (e.keyCode == 16) {
+        $("#shift").addClass("active");
+        $("#shift-2").addClass("active");
+    }
+    if (e.keyCode == 17) {
+        $("#ctrl").addClass("active");
+        $("#ctrl-2").addClass("active");
+    }
+    if (e.keyCode == 18) {
+        $("#alt").addClass("active");
+        $("#alt-2").addClass("active");
+
+    }
+    if (e.keyCode == 19) {
+        $("#pause").addClass("active");
+    }
+    if (e.keyCode == 20) {
+        $("#capslock").addClass("active");
+    }
+    if (e.keyCode == 27) {
+        $("#esc").addClass("active");
+    }
+    if (e.keyCode == 32) {
+        $("#spacebar").addClass("active");
+    }
+    if (e.keyCode == 37) {
+        $("#leftarrow").addClass("active");
+    }
+    if (e.keyCode == 38) {
+        $("#uparrow").addClass("active");
+    }
+    if (e.keyCode == 39) {
+        $("#rightarrow").addClass("active");
+    }
+    if (e.keyCode == 40) {
+        $("#downarrow").addClass("active");
+    }
+    if (e.keyCode == 44) {
+        $("#printscreen").addClass("active");
+    }
+    if (e.keyCode == 45) {
+        $("#insert").addClass("active");
+    }
+    if (e.keyCode == 46) {
+        $("#delete").addClass("active");
+    }
+    if (e.keyCode == 48) {
+        $("#zero").addClass("active");
+    }
+    if (e.keyCode == 49) {
+        $("#one").addClass("active");
+    }
+
+    if (e.keyCode == 50) {
+        $("#two").addClass("active");
+    }
+    if (e.keyCode == 51) {
+        $("#three").addClass("active");
+    }
+    if (e.keyCode == 52) {
+        $("#four").addClass("active");
+    }
+    if (e.keyCode == 53) {
+        $("#five").addClass("active");
+    }
+    if (e.keyCode == 54) {
+        $("#six").addClass("active");
+    }
+    if (e.keyCode == 55) {
+        $("#seven").addClass("active");
+    }
+    if (e.keyCode == 56) {
+        $("#eight").addClass("active");
+    }
+    if (e.keyCode == 57) {
+        $("#nine").addClass("active");
+    }
+    if (e.keyCode == 58) {
+        $("#colon").addClass("active");
+    }
+    if (e.keyCode == 59) {
+        $("#semicolon").addClass("active");
+    }
+
+    if (e.keyCode == 65) {
+        $("#a").addClass("active");
+    }
+    if (e.keyCode == 66) {
+        $("#b").addClass("active");
+    }
+    if (e.keyCode == 67) {
+        $("#c").addClass("active");
+    }
+
+    if (e.keyCode == 68) {
+        $("#d").addClass("active");
+    }
+    if (e.keyCode == 69) {
+        $("#e").addClass("active");
+    }
+    if (e.keyCode == 70) {
+        $("#f").addClass("active");
+    }
+    if (e.keyCode == 71) {
+        $("#g").addClass("active");
+    }
+    if (e.keyCode == 72) {
+        $("#h").addClass("active");
+    }
+    if (e.keyCode == 73) {
+        $("#i").addClass("active");
+    }
+    if (e.keyCode == 74) {
+        $("#j").addClass("active");
+    }
+    if (e.keyCode == 75) {
+        $("#k").addClass("active");
+    }
+    if (e.keyCode == 76) {
+        $("#l").addClass("active");
+    }
+    if (e.keyCode == 77) {
+        $("#m").addClass("active");
+    }
+    if (e.keyCode == 78) {
+        $("#n").addClass("active");
+    }
+    if (e.keyCode == 79) {
+        $("#o").addClass("active");
+    }
+    if (e.keyCode == 80) {
+        $("#p").addClass("active");
+    }
+    if (e.keyCode == 81) {
+        $("#q").addClass("active");
+    }
+    if (e.keyCode == 82) {
+        $("#r").addClass("active");
+    }
+    if (e.keyCode == 83) {
+        $("#s").addClass("active");
+    }
+    if (e.keyCode == 84) {
+        $("#t").addClass("active");
+    }
+    if (e.keyCode == 85) {
+        $("#u").addClass("active");
+    }
+    if (e.keyCode == 86) {
+        $("#v").addClass("active");
+    }
+    if (e.keyCode == 87) {
+        $("#w").addClass("active");
+    }
+    if (e.keyCode == 88) {
+        $("#x").addClass("active");
+    }
+    if (e.keyCode == 89) {
+        $("#y").addClass("active");
+    }
+    if (e.keyCode == 90) {
+        $("#z").addClass("active");
+    }
+
+    if (e.keyCode == 91) {
+        $("#windows").addClass("active");
+    }
+
+    // function keys---------------------------------------
+
+    if (e.keyCode == 112) {
+        $("#f1").addClass("active");
+    }
+    if (e.keyCode == 113) {
+        $("#f2").addClass("active");
+    }
+    if (e.keyCode == 114) {
+        $("#f3").addClass("active");
+    }
+    if (e.keyCode == 115) {
+        $("#f4").addClass("active");
+    }
+    if (e.keyCode == 116) {
+        $("#f5").addClass("active");
+    }
+    if (e.keyCode == 117) {
+        $("#f6").addClass("active");
+    }
+    if (e.keyCode == 118) {
+        $("#f7").addClass("active");
+    }
+    if (e.keyCode == 119) {
+        $("#f8").addClass("active");
+    }
+    if (e.keyCode == 120) {
+        $("#f9").addClass("active");
+    }
+    if (e.keyCode == 121) {
+        $("#f10").addClass("active");
+    }
+    if (e.keyCode == 122) {
+        $("#f11").addClass("active");
+    }
+    if (e.keyCode == 123) {
+        $("#f12").addClass("active");
+    }
+
+
+
+
+    if (e.keyCode == 106) {
+        $("#num-mul").addClass("active");
+    }
+    if (e.keyCode == 111) {
+        $("#num-div").addClass("active");
+    }
+
+    if (e.keyCode == 107) {
+        $("#num-plus").addClass("active");
+    }
+    if (e.keyCode == 109) {
+        $("#num-minus").addClass("active");
+    }
+    if (e.keyCode == 144) {
+        $("#num-lock").addClass("active");
+    }
+
+
+
+    if (e.keyCode == 96) {
+        $("#num-0").addClass("active");
+    }
+    if (e.keyCode == 97) {
+        $("#num-1").addClass("active");
+    }
+    if (e.keyCode == 98) {
+        $("#num-2").addClass("active");
+    }
+
+    if (e.keyCode == 99) {
+        $("#num-3").addClass("active");
+    }
+    if (e.keyCode == 100) {
+        $("#num-4").addClass("active");
+    }
+    if (e.keyCode == 101) {
+        $("#num-5").addClass("active");
+    }
+
+    if (e.keyCode == 102) {
+        $("#num-6").addClass("active");
+    }
+    if (e.keyCode == 103) {
+        $("#num-7").addClass("active");
+    }
+    if (e.keyCode == 104) {
+        $("#num-8").addClass("active");
+    }
+
+    if (e.keyCode == 105) {
+        $("#num-9").addClass("active");
+    }
+    if (e.keyCode == 110) {
+        $("#num-dot").addClass("active");
+    }
+
+    //-------------------------------------------------
+    if (e.keyCode == 189) {
+        $("#minus").addClass("active");
+    }
+    if (e.keyCode == 187) {
+        $("#plus").addClass("active");
+    }
+    if (e.keyCode == 219) {
+        $("#sq-open").addClass("active");
+    }
+    if (e.keyCode == 221) {
+        $("#sq-close").addClass("active");
+    }
+    if (e.keyCode == 220) {
+        $("#back-slash").addClass("active");
+    }
+    if (e.keyCode == 186) {
+        $("#semi-colon").addClass("active");
+    }
+    if (e.keyCode == 222) {
+        $("#comma").addClass("active");
+    }
+    if (e.keyCode == 188) {
+        $("#larrow").addClass("active");
+    }
+    if (e.keyCode == 190) {
+        $("#rarrow").addClass("active");
+    }
+    if (e.keyCode == 191) {
+        $("#slash").addClass("active");
+    }
+
+    if (e.keyCode == 192) {
+        $("#tilt").addClass("active");
+    }
+
+
+});
+
+
+
+
+
+
+
+
+// key up event-----------------------------------------------------------------------------
+window.addEventListener('keyup', function (e) {
+
+
+    if (e.keyCode == 3) {
+        $("#break").removeClass("active");
+    }
+    if (e.keyCode == 8) {
+        $("#backspace").removeClass("active");
+    }
+    if (e.keyCode == 9) {
+        $("#tab").removeClass("active");
+    }
+    if (e.keyCode == 12) {
+        $("#clear").removeClass("active");
+    }
+    if (e.keyCode == 13) {
+        $("#enter").removeClass("active");
+        $("#num-enter").removeClass("active");
+    }
+    if (e.keyCode == 16) {
+        $("#shift").removeClass("active");
+        $("#shift-2").removeClass("active");
+    }
+    if (e.keyCode == 17) {
+        $("#ctrl").removeClass("active");
+        $("#ctrl-2").removeClass("active");
+
+    }
+    if (e.keyCode == 18) {
+        $("#alt").removeClass("active");
+        $("#alt-2").removeClass("active");
+    }
+
+    if (e.keyCode == 19) {
+        $("#pause").removeClass("active");
+    }
+    if (e.keyCode == 20) {
+        $("#capslock").removeClass("active");
+    }
+    if (e.keyCode == 27) {
+        $("#esc").removeClass("active");
+    }
+    if (e.keyCode == 32) {
+        $("#spacebar").removeClass("active");
+    }
+    if (e.keyCode == 37) {
+        $("#leftarrow").removeClass("active");
+    }
+    if (e.keyCode == 38) {
+        $("#uparrow").removeClass("active");
+    }
+    if (e.keyCode == 39) {
+        $("#rightarrow").removeClass("active");
+    }
+    if (e.keyCode == 40) {
+        $("#downarrow").removeClass("active");
+    }
+    if (e.keyCode == 44) {
+        $("#printscreen").removeClass("active");
+    }
+    if (e.keyCode == 45) {
+        $("#insert").removeClass("active");
+    }
+    if (e.keyCode == 46) {
+        $("#delete").removeClass("active");
+    }
+    if (e.keyCode == 48) {
+        $("#zero").removeClass("active");
+    }
+    if (e.keyCode == 49) {
+        $("#one").removeClass("active");
+    }
+
+    if (e.keyCode == 50) {
+        $("#two").removeClass("active");
+    }
+    if (e.keyCode == 51) {
+        $("#three").removeClass("active");
+    }
+    if (e.keyCode == 52) {
+        $("#four").removeClass("active");
+    }
+    if (e.keyCode == 53) {
+        $("#five").removeClass("active");
+    }
+    if (e.keyCode == 54) {
+        $("#six").removeClass("active");
+    }
+    if (e.keyCode == 55) {
+        $("#seven").removeClass("active");
+    }
+    if (e.keyCode == 56) {
+        $("#eight").removeClass("active");
+    }
+    if (e.keyCode == 57) {
+        $("#nine").removeClass("active");
+    }
+    if (e.keyCode == 58) {
+        $("#colon").removeClass("active");
+    }
+    if (e.keyCode == 59) {
+        $("#semicolon").removeClass("active");
+    }
+
+    if (e.keyCode == 65) {
+        $("#a").removeClass("active");
+    }
+    if (e.keyCode == 66) {
+        $("#b").removeClass("active");
+    }
+    if (e.keyCode == 67) {
+        $("#c").removeClass("active");
+    }
+
+    if (e.keyCode == 68) {
+        $("#d").removeClass("active");
+    }
+    if (e.keyCode == 69) {
+        $("#e").removeClass("active");
+    }
+    if (e.keyCode == 70) {
+        $("#f").removeClass("active");
+    }
+    if (e.keyCode == 71) {
+        $("#g").removeClass("active");
+    }
+    if (e.keyCode == 72) {
+        $("#h").removeClass("active");
+    }
+    if (e.keyCode == 73) {
+        $("#i").removeClass("active");
+    }
+    if (e.keyCode == 74) {
+        $("#j").removeClass("active");
+    }
+    if (e.keyCode == 75) {
+        $("#k").removeClass("active");
+    }
+    if (e.keyCode == 76) {
+        $("#l").removeClass("active");
+    }
+    if (e.keyCode == 77) {
+        $("#m").removeClass("active");
+    }
+    if (e.keyCode == 78) {
+        $("#n").removeClass("active");
+    }
+    if (e.keyCode == 79) {
+        $("#o").removeClass("active");
+    }
+    if (e.keyCode == 80) {
+        $("#p").removeClass("active");
+    }
+    if (e.keyCode == 81) {
+        $("#q").removeClass("active");
+    }
+    if (e.keyCode == 82) {
+        $("#r").removeClass("active");
+    }
+    if (e.keyCode == 83) {
+        $("#s").removeClass("active");
+    }
+    if (e.keyCode == 84) {
+        $("#t").removeClass("active");
+    }
+    if (e.keyCode == 85) {
+        $("#u").removeClass("active");
+    }
+    if (e.keyCode == 86) {
+        $("#v").removeClass("active");
+    }
+    if (e.keyCode == 87) {
+        $("#w").removeClass("active");
+    }
+    if (e.keyCode == 88) {
+        $("#x").removeClass("active");
+    }
+    if (e.keyCode == 89) {
+        $("#y").removeClass("active");
+    }
+    if (e.keyCode == 90) {
+        $("#z").removeClass("active");
+    }
+
+    if (e.keyCode == 91) {
+        $("#windows").removeClass("active");
+    }
+
+    // function keys---------------------------------------
+
+    if (e.keyCode == 112) {
+        $("#f1").removeClass("active");
+    }
+    if (e.keyCode == 113) {
+        $("#f2").removeClass("active");
+    }
+    if (e.keyCode == 114) {
+        $("#f3").removeClass("active");
+    }
+    if (e.keyCode == 115) {
+        $("#f4").removeClass("active");
+    }
+    if (e.keyCode == 116) {
+        $("#f5").removeClass("active");
+    }
+    if (e.keyCode == 117) {
+        $("#f6").removeClass("active");
+    }
+    if (e.keyCode == 118) {
+        $("#f7").removeClass("active");
+    }
+    if (e.keyCode == 119) {
+        $("#f8").removeClass("active");
+    }
+    if (e.keyCode == 120) {
+        $("#f9").removeClass("active");
+    }
+    if (e.keyCode == 121) {
+        $("#f10").removeClass("active");
+    }
+    if (e.keyCode == 122) {
+        $("#f11").removeClass("active");
+    }
+    if (e.keyCode == 123) {
+        $("#f12").removeClass("active");
+    }
+
+    if (e.keyCode == 106) {
+        $("#num-mul").removeClass("active");
+    }
+    if (e.keyCode == 111) {
+        $("#num-div").removeClass("active");
+    }
+
+    if (e.keyCode == 107) {
+        $("#num-plus").removeClass("active");
+    }
+    if (e.keyCode == 109) {
+        $("#num-minus").removeClass("active");
+    }
+    if (e.keyCode == 144) {
+        $("#num-lock").removeClass("active");
+    }
+
+
+
+    if (e.keyCode == 96) {
+        $("#num-0").removeClass("active");
+    }
+    if (e.keyCode == 97) {
+        $("#num-1").removeClass("active");
+    }
+    if (e.keyCode == 98) {
+        $("#num-2").removeClass("active");
+    }
+
+    if (e.keyCode == 99) {
+        $("#num-3").removeClass("active");
+    }
+    if (e.keyCode == 100) {
+        $("#num-4").removeClass("active");
+    }
+    if (e.keyCode == 101) {
+        $("#num-5").removeClass("active");
+    }
+
+    if (e.keyCode == 102) {
+        $("#num-6").removeClass("active");
+    }
+    if (e.keyCode == 103) {
+        $("#num-7").removeClass("active");
+    }
+    if (e.keyCode == 104) {
+        $("#num-8").removeClass("active");
+    }
+    if (e.keyCode == 105) {
+        $("#num-9").removeClass("active");
+    }
+    if (e.keyCode == 110) {
+        $("#num-dot").removeClass("active");
+    }
+
+
+    //-------------------------------------------------
+    if (e.keyCode == 189) {
+        $("#minus").removeClass("active");
+    }
+    if (e.keyCode == 187) {
+        $("#plus").removeClass("active");
+    }
+    if (e.keyCode == 219) {
+        $("#sq-open").removeClass("active");
+    }
+    if (e.keyCode == 221) {
+        $("#sq-close").removeClass("active");
+    }
+    if (e.keyCode == 220) {
+        $("#back-slash").removeClass("active");
+    }
+    if (e.keyCode == 186) {
+        $("#semi-colon").removeClass("active");
+    }
+    if (e.keyCode == 222) {
+        $("#comma").removeClass("active");
+    }
+    if (e.keyCode == 188) {
+        $("#larrow").removeClass("active");
+    }
+    if (e.keyCode == 190) {
+        $("#rarrow").removeClass("active");
+    }
+    if (e.keyCode == 191) {
+        $("#slash").removeClass("active");
+    }
+
+    if (e.keyCode == 192) {
+        $("#tilt").removeClass("active");
+    }
+
+
+
+});
