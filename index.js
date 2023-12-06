336ff55d92db1be73e37cdf8479b127c6a1f52bd
i73405 <Abhishek.Kumar@microchip.com>
2023-12-06 10:36:31 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ window.addEventListener('keydown', function (e) {
 
     if (e.keyCode == 3) {
         $("#break").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 8) {
         $("#backspace").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 9) {
         $("#tab").addClass("active");
@@ -14,287 +14,287 @@ window.addEventListener('keydown', function (e) {
     }
     if (e.keyCode == 12) {
         $("#clear").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 13) {
         $("#enter").addClass("active");
-        e.preventDefault();
+        
         $("#num-enter").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 16) {
         $("#shift").addClass("active");
-        e.preventDefault();
+        
         $("#shift-2").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 17) {
         $("#ctrl").addClass("active");
-        e.preventDefault();
+        
         $("#ctrl-2").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 18) {
         $("#alt").addClass("active");
-        e.preventDefault();
+        
         $("#alt-2").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 19) {
         $("#pause").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 20) {
         $("#capslock").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 27) {
         $("#esc").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 32) {
         $("#spacebar").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 37) {
         $("#leftarrow").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 38) {
         $("#uparrow").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 39) {
         $("#rightarrow").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 40) {
         $("#downarrow").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 44) {
         $("#printscreen").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 45) {
         $("#insert").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 46) {
         $("#delete").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 48) {
         $("#zero").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 49) {
         $("#one").addClass("active");
-        e.preventDefault();
+        
     }
 
     if (e.keyCode == 50) {
         $("#two").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 51) {
         $("#three").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 52) {
         $("#four").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 53) {
         $("#five").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 54) {
         $("#six").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 55) {
         $("#seven").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 56) {
         $("#eight").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 57) {
         $("#nine").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 58) {
         $("#colon").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 59) {
         $("#semicolon").addClass("active");
-        e.preventDefault();
+        
     }
 
     if (e.keyCode == 65) {
         $("#a").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 66) {
         $("#b").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 67) {
         $("#c").addClass("active");
-        e.preventDefault();
+        
     }
 
     if (e.keyCode == 68) {
         $("#d").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 69) {
         $("#e").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 70) {
         $("#f").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 71) {
         $("#g").addClass("active");
-        e.preventDefault();
+        
 
     }
     if (e.keyCode == 72) {
         $("#h").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 73) {
         $("#i").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 74) {
         $("#j").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 75) {
         $("#k").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 76) {
         $("#l").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 77) {
         $("#m").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 78) {
         $("#n").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 79) {
         $("#o").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 80) {
         $("#p").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 81) {
         $("#q").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 82) {
         $("#r").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 83) {
         $("#s").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 84) {
         $("#t").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 85) {
         $("#u").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 86) {
         $("#v").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 87) {
         $("#w").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 88) {
         $("#x").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 89) {
         $("#y").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 90) {
         $("#z").addClass("active");
-        e.preventDefault();
+        
     }
 
     if (e.keyCode == 91) {
         $("#windows").addClass("active");
-        e.preventDefault();
+        
     }
 
     // function keys---------------------------------------
 
     if (e.keyCode == 112) {
         $("#f1").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 113) {
         $("#f2").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 114) {
         $("#f3").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 115) {
         $("#f4").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 116) {
         $("#f5").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 117) {
         $("#f6").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 118) {
         $("#f7").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 119) {
         $("#f8").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 120) {
         $("#f9").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 121) {
         $("#f10").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 122) {
         $("#f11").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 123) {
         $("#f12").addClass("active");
-        e.preventDefault();
+        
     }
 
 
@@ -302,117 +302,117 @@ window.addEventListener('keydown', function (e) {
 
     if (e.keyCode == 106) {
         $("#num-mul").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 111) {
         $("#num-div").addClass("active");
-        e.preventDefault();
+        
     }
 
     if (e.keyCode == 107) {
         $("#num-plus").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 109) {
         $("#num-minus").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 144) {
         $("#num-lock").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 96) {
         $("#num-0").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 97) {
         $("#num-1").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 98) {
         $("#num-2").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 99) {
         $("#num-3").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 100) {
         $("#num-4").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 101) {
         $("#num-5").addClass("active");
-        e.preventDefault();
+        
     }
 
     if (e.keyCode == 102) {
         $("#num-6").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 103) {
         $("#num-7").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 104) {
         $("#num-8").addClass("active");
-        e.preventDefault();
+        
     }
 
     if (e.keyCode == 105) {
         $("#num-9").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 110) {
         $("#num-dot").addClass("active");
-        e.preventDefault();
+        
     }
 
     //-------------------------------------------------
     if (e.keyCode == 189) {
         $("#minus").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 187) {
         $("#plus").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 219) {
         $("#sq-open").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 221) {
         $("#sq-close").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 220) {
         $("#back-slash").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 186) {
         $("#semi-colon").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 222) {
         $("#comma").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 188) {
         $("#larrow").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 190) {
         $("#rarrow").addClass("active");
-        e.preventDefault();
+        
     }
     if (e.keyCode == 191) {
         $("#slash").addClass("active");
-        e.preventDefault();
+        
     }
 
     if (e.keyCode == 192) {
         $("#tilt").addClass("active");
-        e.preventDefault();
+        
     }
 });
 
